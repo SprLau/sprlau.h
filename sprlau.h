1803ed8b94e782acc4ebad36f8cb618ad3a14844
SprLau <lau@buaa.edu.cn>
2021-09-30 20:08:40 +0800

    update bits/stdc++.h added

--- sprlau.h
+++ sprlau.h
@@ -1,13 +1,92 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<time.h>
-#include<ctype.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
-#define Max(a,b)    a > b ? a : b
-#define Min(a,b)    a < b ? a : b
-#define MaxSize     1000
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#define MAX(a,b)    a > b ? a : b
+#define MIN(a,b)    a < b ? a : b
+#define MAX_SIZE     1000
 //qsort(a, 6, sizeof(*a), cmp);
 
 typedef long long int LLI;
@@ -27,13 +106,13 @@ typedef struct chain {
 } CHAIN;
 
 typedef struct queue {
-    int Queue[MaxSize];
+    int Queue[MAX_SIZE];
     int frt;
     int rear;
 } QUEUE;
 
 typedef struct stack {
-    int Stack[MaxSize];
+    int Stack[MAX_SIZE];
     int cur;
 } STACK;
 
@@ -187,7 +266,7 @@ void DeleteTwig(TREE* bt, int key)
 }
 
 typedef struct LOPQ {
-    TREE* numQ[MaxSize];
+    TREE* numQ[MAX_SIZE];
     int front;
     int rear;
 } LOPQUEUE;
